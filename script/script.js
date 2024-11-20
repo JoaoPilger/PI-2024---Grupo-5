eb72cde431fb4fc86053c94357437e9853a14b7e
JoaoPilger <joaovictorpilger@gmail.com>
2024-11-20 13:19:49 -0300

    pequenos consertos

--- script/script.js
+++ script/script.js
@@ -99,6 +99,21 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 // AVALIAÇÃO
+const button = document.getElementById("buttonAval")
+
+button.addEventListener("click", () =>{
+  const modal = document.getElementById("modal")
+  const darkground = document.getElementById("darkground")
+
+  darkground.style.backgroundColor = "rgba(0, 0, 0, 0.4)"
+
+  modal.style.display = "flex"
+  modal.style.position = "fixed"
+  modal.style.top = "50%"
+  modal.style.left = "50%"
+  modal.style.transform = "translate(-50%,-50%)"
+})
+
 
 // SOBRE NÓS
 const content = [
