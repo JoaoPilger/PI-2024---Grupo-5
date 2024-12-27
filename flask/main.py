32a966a92bb82a2cc8188a2509e73413a2866c0d
jappejappe <gabrieljappemoura95071@gmail.com>
2024-12-27 15:38:17 -0300

    print 'GenZ Dynamics' on terminal

--- flask/main.py
+++ flask/main.py
@@ -1,7 +1,37 @@
 from flask import Flask, render_template
 
+def rgb_text(r, g, b, text):
+    return f"\033[38;2;{r};{g};{b}m{text}\033[0m"
+
 # Cria a aplicação Flask
 app = Flask(__name__)
+
+print(rgb_text(171, 248, 202,'''                                                                                                                        
+                                lkkkkkkkkkkx,                                okkkkkkkkkkko                              
+                               :X0.       cXK                                       .oXX:                               
+                               cXO               ,dkkkkkkx;    xk,dkkkkko.          lX0                                 
+                               cXO     ......    0X,    'XK.   KX'    .xXo        .OX:                                  
+                               cXO    dKKKKXK    KXc;;;;:XX.   KX.     dXo       cX0                                    
+                               cXO        'XK    KXo.......    KX.     dXo     .kXc                                     
+                               cXO........;XK    KX.           KX.     dXo    :KX:........                              
+                                dKKKKKKKKKK0'    ,0KKKKKKK0.   0K.     oKl   dKKKKKKKKKKK0                              ''')),
+print(rgb_text(255, 255, 255,'''                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+                                                                                        .'.                             
+         0NNNNNNNNNXx'                                                                  cxc                             
+         XM,        OMx                                                                                                 
+         XM,        .MW   WW      NM.  0MKXMMMMMNd    kMMMMMMMNo   ,MMXWMMMW0oNMMMWX.   lMd   .XMMMMMMMN;   cNMMMMMMMl  
+         XM,        .MW   .MN    WM'   0M;     .MN           .MN   ,M0     NM.    OMc   lMd   oMx     lMk   KM,         
+         XM,        .MW    .M0  KM,    0M'     .MN    xXXXXXXWMN   ,M0     NM.    OMc   lMd   oMx           lMXkkkkkk'  
+         XM,        lMX     :MOkM:     0M'     .MN   .MK     'MN   ,M0     NM.    OMc   lMd   oMx     ...          .MN  
+         XMX,,,,,,lOMK       cMMc      0M'     .MN   .MMc,,,,kMN   ,M0     NM.    OMc   lMd   lMM,,,,,NMk   .,,,,,,OMX  
+                             oMd                                                                                        
+                            'Mx                                                                                         
+                           .MK                                                                                          
+                                                                                                                        
+'''))
 
 # Define a rota para a página inicial
 @app.route('/')
@@ -14,4 +44,4 @@ def avaliacao():
 
 # Inicia o servidor Flask se este arquivo for executado diretamente
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
--- flask/sistema/gemini.py
+++ flask/sistema/gemini.py
@@ -18,20 +18,20 @@ prompt = f"Crie uma rotina de exercícios físicos com no máximo 2 linhas"
 print("carregando")
 response = model.generate_content(prompt)
 
-artigos = ["./sistema/artigos/atividade_fisica_saude_mental_e_reabilitacao_psicossocial.pdf",
-    "./sistema/artigos/beneficios_da_atividade_fisica_e_do_exercicio_fisico_na_depressao.pdf",
-    "./sistema/artigos/conhecimento_de_adolescentes_acerca_dos_beneficios_do_exercicio_fisico_para_a_saude_mental.pdf",
-    "./sistema/artigos/efeito_da_atividade_fisica_associada_a_orientacao_alimentar_em_adolescentes_obesos_comparacao_entre_o_exercicio_aerobio_e_anaerobio.pdf",
-    "./sistema/artigos/efeitos_dos_exercicios_fisicos_em_criancas_e_adolescentes.pdf",
-    "./sistema/artigos/efeitos_psicofisiologicos_do_exercicio_fisico_em_pacientes_com_transtornos_de_ansiedade_e_depressao.pdf",
-    "./sistema/artigos/exercicio_comportamento_alimentar_e_obesidade_revisao_dos_efeitos_sobre_a_composicao_corpoera_e_parametros_metabolicos.pdf",
-    "./sistema/artigos/exercicio_fisico_e_obesidade_morbida_uma_revisao_sistematica.pdf",
-    "./sistema/artigos/exercicio_fisico_e_sindrome_metabolica.pdf",
-    "./sistema/artigos/influencia_da_educacao_fisica_na_saude_mental_de_estudantes_universitarios.pdf",
-    "./sistema/artigos/o_efeito_antidepressivo_do_exercicio_fisico.pdf",
-    "./sistema/artigos/o_exercicio_fisico_e_os_aspectos_psicobiologicos.pdf",
-    "./sistema/artigos/obesidade_em_adolescentes_e_as_politicas_publicas_de_nutricao.pdf", 
-    "./sistema/artigos/prescricao_de_exercicios_fisicos_por_inteligencia_artificial_a_educacao_fisica_vai_acabar.pdf"]
+artigos = ["./flask/sistema/artigos/atividade_fisica_saude_mental_e_reabilitacao_psicossocial.pdf",
+    "./flask/sistema/artigos/beneficios_da_atividade_fisica_e_do_exercicio_fisico_na_depressao.pdf",
+    "./flask/sistema/artigos/conhecimento_de_adolescentes_acerca_dos_beneficios_do_exercicio_fisico_para_a_saude_mental.pdf",
+    "./flask/sistema/artigos/efeito_da_atividade_fisica_associada_a_orientacao_alimentar_em_adolescentes_obesos_comparacao_entre_o_exercicio_aerobio_e_anaerobio.pdf",
+    "./flask/sistema/artigos/efeitos_dos_exercicios_fisicos_em_criancas_e_adolescentes.pdf",
+    "./flask/sistema/artigos/efeitos_psicofisiologicos_do_exercicio_fisico_em_pacientes_com_transtornos_de_ansiedade_e_depressao.pdf",
+    "./flask/sistema/artigos/exercicio_comportamento_alimentar_e_obesidade_revisao_dos_efeitos_sobre_a_composicao_corpoera_e_parametros_metabolicos.pdf",
+    "./flask/sistema/artigos/exercicio_fisico_e_obesidade_morbida_uma_revisao_sistematica.pdf",
+    "./flask/sistema/artigos/exercicio_fisico_e_sindrome_metabolica.pdf",
+    "./flask/sistema/artigos/influencia_da_educacao_fisica_na_saude_mental_de_estudantes_universitarios.pdf",
+    "./flask/sistema/artigos/o_efeito_antidepressivo_do_exercicio_fisico.pdf",
+    "./flask/sistema/artigos/o_exercicio_fisico_e_os_aspectos_psicobiologicos.pdf",
+    "./flask/sistema/artigos/obesidade_em_adolescentes_e_as_politicas_publicas_de_nutricao.pdf", 
+    "./flask/sistema/artigos/prescricao_de_exercicios_fisicos_por_inteligencia_artificial_a_educacao_fisica_vai_acabar.pdf"]
 
 all_docs = [] # conhecimento dos artigos acima
 
